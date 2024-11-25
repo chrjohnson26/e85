5fd96afc179295927b8357602c4c91f5f89e0b54
Christian Johnson <131117236+chrjohnson26@users.noreply.github.com>
2024-11-24 22:21:41 -0800

    Updating multicycle_controller.sv

--- lab10/multicycle_controller.sv
+++ lab10/multicycle_controller.sv
@@ -2,11 +2,11 @@
 // risc-v multicycle controller
 // Christian Johnson
 // chrjohnson@hmc.edu
-// 11/23/2024
+// 11/24/2024
 
 // Structural Verilog Code for the ALU controller
 
-module controller(input logic clk,
+module multicycle_controller(input logic clk,
 						 input logic reset, 
 						 input logic [6:0] op,
 						 input logic [2:0] funct3,
@@ -20,30 +20,34 @@ module controller(input logic clk,
 						 output logic irwrite, pcwrite, 
 						 output logic regwrite, memwrite);
 		
+		// Defining internal logic for the multicycle controller
 		logic pcupdate, branch;	
 		logic [1:0] aluop;
 		
+		// Calling mainFSM module
 		mainFSM fsm(clk, reset, op, pcupdate, branch, regwrite, memwrite, irwrite, resultsrc, alusrca, alusrcb, adrsrc, aluop);
 		
 		
-		// aluop[1]: a
-		// aluop[0]: b
+		/*
+		Calling alu_decoder (from lab 2 implementation)
+		aluop[1]: a
+		aluop[0]: b
 		
-		// funct3[2]: c
-		// funct3[1]: d
-		// funct3[0]: e
+		funct3[2]: c
+		funct3[1]: d
+		funct3[0]: e
 		
-		// op[5]: 	 f
-		// funct7b5: g
+		op[5]: 	 f
+		funct7b5: g
+		*/
 		alu_decoder aludec(aluop[1], aluop[0], funct3[2], funct3[1], funct3[0], op[5], funct7b5, alucontrol[2], alucontrol[1], alucontrol[0]);
 		
+		// Calling instr_decoder module
 		instr_decoder id(op, immsrc);
 		
 							
 		// output logic
 		assign pcwrite = branch & zero | pcupdate;
-		
-
 endmodule
 
 
@@ -62,120 +66,96 @@ module mainFSM(input logic clk,
 					
 					
 		// Internal logic for states
-		typedef enum logic [3:0] {S0, S1, S2, S3, S4, S5, S6, S7, S8, S9, S10} statetype;
+		typedef enum logic [3:0] {FETCH, DECODE, MEMADR, MEMREAD, MEMWB, MEMWRITE, EXECUTER, ALUWB, EXECUTEI, JAL, BEQ} statetype;
 		statetype state, nextstate;
+		logic [13:0] controls;
 		
 		// state register
 		always_ff @(posedge clk, posedge reset)
-			if (reset) state <= S0;
+			if (reset) state <= FETCH;
 			else state <= nextstate;
 			
 		
 		// next state logic
 		always_comb
+			casez (state)
+				FETCH:		nextstate = DECODE;
+				
+				DECODE:		casez(op)
+									7'b0?00011:			nextstate = MEMADR;
+									7'b0110011:			nextstate = EXECUTER;
+									7'b0010011:			nextstate = EXECUTEI;
+									7'b1101111:			nextstate = JAL;
+									7'b1100011:			nextstate = BEQ;
+									default:				nextstate = state;
+								endcase
+									
+				MEMADR:		casez(op)
+									7'b0000011:			nextstate = MEMREAD;
+									7'b0100011:			nextstate = MEMWRITE;
+									default:				nextstate = state;
+								endcase
+				
+				MEMREAD:		nextstate = MEMWB;
+				
+				MEMWB:		nextstate = FETCH;
+				
+				MEMWRITE:	nextstate = FETCH;
+				
+				EXECUTER:	nextstate = ALUWB;
+				
+				ALUWB:		nextstate = FETCH;
+				
+				EXECUTEI:   nextstate = ALUWB;
+				
+				JAL:			nextstate = ALUWB;
+				
+				BEQ:			nextstate = FETCH;
+				
+				default:		nextstate = state;
+		endcase
+		
+	
+				
+		// setting current state signals
+		always_comb
 			case (state)
-				S0:		begin
-							nextstate = S1;
-							adrsrc  = 1'b0;
-							irwrite = 1'b1;
-							alusrca = 2'b00;
-							alusrcb = 2'b10;
-							aluop = 2'b00;
-							resultsrc = 2'b10;
-							pcupdate = 1'b1;
-				end
-							
-				S1:		begin
-							if ((op == 7'b0000011) | (op == 7'b0100011))		nextstate = S2;
-							else if (op == 7'b0110011) 							nextstate = S6;
-							else if (op == 7'b0010011) 							nextstate = S8;
-							else if (op == 7'b1101111) 							nextstate = S9;
-							else 	  														nextstate = S10;
-							alusrca = 2'b01;
-							alusrcb = 2'b01;
-							aluop   = 2'b00;
-							resultsrc = 2'b10;
-				end
-							
-				S2:		begin
-							if 	  (op == 7'b0000011)		nextstate = S3;
-							else	  							 	nextstate = S5;
-							alusrca = 2'b10;
-							alusrcb = 2'b01;
-							aluop   = 2'b00;
-							resultsrc = 2'b10;
-				end
-							
-				S3: 		begin
-							nextstate = S4;
-							resultsrc = 2'b00;
-							adrsrc    = 1'b1;
-				end
+						// pcupdate__branch__regwrite__memwrite__irwrite__resultsrc__alusrcb__alusrca__adrsrc__aluop
+				FETCH: 		controls = 14'b1_0_0_0_1_10_10_00_0_00;
 				
-				S4:		begin
-							nextstate = S0;
-							resultsrc = 2'b01;
-							regwrite  = 1'b1;
-				end
-							
-				S5: 		begin
-							nextstate = S0;
-							resultsrc = 2'b00;
-							adrsrc    = 1'b1;
-							memwrite  = 1'b1;
-				end
-							
-				S6: 		begin
-							nextstate = S7;
-							alusrca   = 2'b10;
-							alusrcb   = 2'b00;
-							aluop     = 2'b10;
-							resultsrc = 2'b10;
-				end
-							
-				S7: 		begin
-							nextstate = S0;
-							resultsrc = 2'b00;
-							regwrite  = 1'b1;
-				end
-							
-				S8:		begin
-							nextstate = S7;
-							alusrca 	 = 2'b10;
-							alusrcb   = 2'b01;
-							aluop		 = 2'b10;
-							resultsrc = 2'b10;
-				end
+				DECODE:		controls = 14'b0_0_0_0_0_00_01_01_0_00;
 				
-				S9: 		begin
-							nextstate = S7;
-							resultsrc = 2'b00;
-							regwrite  = 1'b1;
-				end
-				
-				S10: 		begin
-							nextstate = S0;
-							alusrca   = 2'b10;
-							alusrcb   = 2'b00;
-							aluop     = 2'b01;
-							resultsrc = 2'b00;
-							branch    = 1'b1;
-				end
-				default:	nextstate = state;
-		endcase					
+				MEMADR:		controls = 14'b0_0_0_0_0_00_01_10_0_00;
+					
+				MEMREAD: 	controls = 14'b0_0_0_0_0_00_00_00_1_00;
+					
+				MEMWB:		controls = 14'b0_0_1_0_0_01_00_00_0_00;
+					
+				MEMWRITE:	controls = 14'b0_0_0_1_0_00_00_00_1_00;
+					
+				EXECUTER:	controls = 14'b0_0_0_0_0_00_00_10_0_10;
+					
+				ALUWB:		controls = 14'b0_0_1_0_0_00_00_00_0_00;
+					
+				EXECUTEI:	controls = 14'b0_0_0_0_0_00_01_10_0_10;
+					
+				JAL:			controls = 14'b1_0_0_0_0_00_10_01_0_00;
+					
+				BEQ:			controls = 14'b0_1_0_0_0_00_00_10_0_01;
+					
+				default:		controls = 14'b0_0_0_0_0_00_00_00_0_00;
+		endcase
+			
+			assign {pcupdate, branch, regwrite, memwrite, irwrite, resultsrc, alusrcb, alusrca, adrsrc, aluop} = controls;
 endmodule
 		
 			
 		
 
 						 
-// Structural Verilog Code for the ALU Decoder
-
-module alu_decoder(input  logic a, b, c, d, e, f, g,
-						 output logic y2, y1, y0);
-		
-
-		
+// Structural Verilog Code for the ALU Decoder adapted from lab 2
+module alu_decoder(input  logic a, b, c, d, e, f, g, 
+						output logic y2, y1, y0);
 		// Declaring the internal logic signals or local variables
 		// which can only be used inside of this module
 		logic n1, n2, n3, na, nb, nc, nd, ne;
@@ -198,7 +178,6 @@ module alu_decoder(input  logic a, b, c, d, e, f, g,
 		and a4(n2, a, nb, nc, nd, ne, f, g);
 		and a5(n3, a, nb, d, ne);
 		or o1(y0, n1, n2, n3);
-		
 endmodule
 
 
@@ -211,8 +190,7 @@ module instr_decoder(input logic [6:0] op,
 							
 		// Logic for choosing the immsrc
 		always_comb
-			casez(op)
-								// regwrite_immsrc_alusrc_branch_memwrite_memtoreg_jump_aluop
+			casez (op)
 				7'b0110011: immsrc <= 2'b00; // R-type data processing (dont care but setting to 0's)
 				7'b0010011: immsrc <= 2'b00; // I-type data processing
 				7'b0000011: immsrc <= 2'b00; // LW
